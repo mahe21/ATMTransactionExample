8b98d3b170010cb62a0dceb4cb11582dd93f5621
Mahesh <Maheshk2104@gmail.com>
2023-07-14 08:00:37 +0530

    [Mahesh]Add: Initial commit

---
+++ Atm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmTransaction
+{
+    public class Atm
+    {
+        private int accountBalance;
+        
+
+
+        public Atm(int accountBalance)
+        {
+            this.accountBalance = accountBalance;            
+        }
+
+
+        public void CheckBalance()
+        {
+            Console.WriteLine("Your account balance is: " + accountBalance + "Rs");
+        }
+
+        public void Withdrawal(int amount)
+        {
+            if (amount % 100 == 0)
+            {
+                if (amount <= accountBalance)
+                {
+                    accountBalance -= amount;
+                    Console.WriteLine("Withdrawal successful, Remaining balance is: " + accountBalance + "Rs");
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance, please enter valid amount/ enter amount in 100's\"");
+                }
+            }
+            
+        }
+
+        public void Deposit(int amount)
+        {
+            if (amount % 100 == 0)
+            {
+                if (amount <= accountBalance)
+                {
+                    accountBalance += amount;
+                    Console.WriteLine("Withdrawal successful, Remaining balance is: " + accountBalance + "Rs");
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance, please enter valid amount/ enter amount in 100's");
+                }
+
+            }               
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace AtmTransaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int accountBalance = 1000;
+
+
+            Atm atm = new Atm(accountBalance);
+
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Make a Withdrawal");
+            Console.WriteLine("3. Make a Deposit");
+            Console.WriteLine("4. Exit");
+
+
+            while (true)
+            {
+                Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        atm.CheckBalance();
+                        break;
+
+                    case 2:
+                        Console.Write("Enter the withdrawal amount: ");
+                        int withdrawalAmount = Convert.ToInt32(Console.ReadLine());
+                        atm.Withdrawal(withdrawalAmount);
+                        break;
+
+                    case 3:
+                        Console.Write("Enter the Deposit amount: ");
+                        int depositAmount = Convert.ToInt32(Console.ReadLine());
+                        atm.Deposit(depositAmount);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thank you for using the ATM.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+            }
+        }
+    }
+}
